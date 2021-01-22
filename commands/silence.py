d306ab9d878edd0804ba6b662422ca5adea1d502
Max Cieplinski <max.cieplinski@gmail.com>
2021-01-22 18:33:46 +0100

    0.1

--- commands/silence.py
+++ commands/silence.py
@@ -2,29 +2,29 @@ import discord
 from discord.ext import commands
 
 class Silence(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+	def __init__(self, client):
+		self.client = client
 
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def silence(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        await ctx.message.add_reaction('✅')
+	@commands.command()
+	@commands.has_permissions(ban_members=True)
+	async def silence(self, ctx):
+		await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
+		await ctx.message.add_reaction('✅')
 
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def unsilence(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-        await ctx.message.add_reaction('✅')
+	@commands.command()
+	@commands.has_permissions(ban_members=True)
+	async def unsilence(self, ctx):
+		await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+		await ctx.message.add_reaction('✅')
 
-@silence.error
+	@silence.error
 	async def silence_error(self, ctx, error):
 		if ctx.message.author.guild_permissions.ban_members:
 			await ctx.message.add_reaction('❌')
 		else:
 			pass
 
-@silence.error
+	@silence.error
 	async def unsilence_error(self, ctx, error):
 		if ctx.message.author.guild_permissions.ban_members:
 			await ctx.message.add_reaction('❌')
@@ -32,4 +32,4 @@ class Silence(commands.Cog):
 			pass
 
 def setup(client):
-    client.add_cog(Silence(client))
+	client.add_cog(Silence(client))
