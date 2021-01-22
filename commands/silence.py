4b0d6e4a97cfdd6fc6326c2a1b73a6a7719cca1c
Max Cieplinski <max.cieplinski@gmail.com>
2021-01-22 18:30:24 +0100

    0.1

--- commands/silence.py
+++ commands/silence.py
@@ -9,11 +9,27 @@ class Silence(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def silence(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
+        await ctx.message.add_reaction('✅')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unsilence(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+        await ctx.message.add_reaction('✅')
+
+@silence.error
+	async def silence_error(self, ctx, error):
+		if ctx.message.author.guild_permissions.ban_members:
+			await ctx.message.add_reaction('❌')
+		else:
+			pass
+
+@silence.error
+	async def unsilence_error(self, ctx, error):
+		if ctx.message.author.guild_permissions.ban_members:
+			await ctx.message.add_reaction('❌')
+		else:
+			pass
 
 def setup(client):
     client.add_cog(Silence(client))
